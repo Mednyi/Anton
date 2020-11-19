9db59f91a5ead9fa08cfcffccdf0c54be5895a22
mytestkube <mednyi.vsadnik@gmail.com>
2020-11-19 20:51:04 +0300

    'operators'

---
+++ operators.js
@@ -0,0 +1,43 @@
+// Operators
+// Binary and Unary
+// Arithmetic
+/*
+Binary
+a - b - a,b - operands
+a + b
+a * b
+a / b
+a ** b
+a % b
+a = b
+a += b // equivalent a = a + b
+a -= b // equivalent a = a - b
+a /= b // equivalent a = a / b
+a *= b // equivalent a = a * b
+ */
+/*
+Unary
++a - coverts string to numbers
+-a - converts string to number with opposite sign
+a++ - postfix increment
+++a - prefix increment
+a-- - postfix decrement
+--a - prefix decrement
+ */
+// Comparison operators
+/*
+a == b - not strict comparison
+a === b - strict comparison
+a > b
+a < b
+a >= b
+a <= b
+a != b
+a !== b
+ */
+// Logical operators
+/*
+!a
+a && b
+a || b
+ */
