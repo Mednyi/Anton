47427b28dcc298ed48228655b1ae05f0a4baec5c
ministrov <61511030+ministrov@users.noreply.github.com>
2020-12-16 14:41:17 +0300

    practising

--- fourthProgramm.js
+++ fourthProgramm.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const numberArr = [23, 34, 456, 31, 45, 567, 321, 907, 60, 78];
+const numbersArr = [23, 34, 456, 31, 45, 567, 321, 907, 60, 78];
 
 function bubbleSort (arr) {
   for (let j = numberArr.length - 1; j > 0; j--) {
@@ -11,5 +11,21 @@ function bubbleSort (arr) {
         numberArr[i + 1] = temp;
       }
     }
+  }
+}
+
+const newArr10 = [9, 1, 9, 29, 45, 45, 560];
+const min = 0;
+for (let i = 0; i < newArr10.length; i++) {
+  if (newArr10[i] < min) {
+    min = newArr10[i];
+  }
+}
+
+const newArr15 = [9, 1, 9, 29, 45, 45, 560];
+const max = 0;
+for (let i = 0; i < newArr15.length; i++) {
+  if (newArr15[i] < max) {
+    max = newArr15;
   }
 }
