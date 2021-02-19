81029c14fb89452221febc8ba1b0a6887fff78a5
ministrov <61511030+ministrov@users.noreply.github.com>
2021-02-19 21:15:56 +0300

    trained creating DOM element

---
+++ DOMelement.js
@@ -0,0 +1,32 @@
+const myTraining = document.querySelector('.button__training');
+myTraining.addEventlistener('click', function () {
+  buttonClose();
+  myTraining.removeEventListener();
+});
+
+buttonClose.addEventlistener = function () {
+  closeBigPicture();
+  document.removeEventListener('click');
+}
+
+// Создаю ДОМ элементы в ручную
+
+const renderListItem = function (element) {
+  const listItem = document.createElement('li');
+  const image = document.createElement('img');
+  const text = document.createElement('p');
+  listItem.appendChild(image);
+  listItem.appendChild(text);
+  image.src = element.url;
+  text.textContent = element.text;
+  return listItem;
+}
+
+const renderList = function (animals) {
+  const list = document.createElement('ul');
+  const fragment = document.createDocumentFragment();
+  animals.forEach((animal) => {
+    fragment.appendChild(renderListItem(animal));
+  });
+  list.appendChild(fragment);
+}
