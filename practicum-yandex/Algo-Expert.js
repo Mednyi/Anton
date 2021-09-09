15860a6b347a7bcaa5e250e61883d18dd61ae837
ministrov <61511030+ministrov@users.noreply.github.com>
2021-09-09 15:28:13 +0300

    Add algo-expert file

--- practicum-yandex/Algo-Expert.js
+++ practicum-yandex/Algo-Expert.js
@@ -1,3 +1,6 @@
+// First task
+
+
 const array = [3,5,-4,8,11,1,-1,6];
 const targetSum = 10;
 
@@ -13,5 +16,24 @@ function twoNumberSum(array, targetSum) {
   // Write your code here.
 }
 
+
+//  Secod task
+
+function isValidSubsequence(array, sequence) {
+    let j = 0;
+    for (let i = 0; i < array.length; i++)  {
+        if (sequence[j] === array[i]) {
+            j++;
+        }
+    }
+  // Write your code here.
+  return j === sequence.length;
+
+}
+
+
+const array = [5,1,22,25,6,-1,8,10]
+
+const sequence = [1,6,-1,10]
 
 
