087c10a74c26d014cd19620741e5a2c730fc2eb5
Ministrov <ministrov2018@gmail.com>
2021-09-16 18:44:39 +0300

    Resolve the task from codewars

--- practicum-yandex/codewars.js
+++ practicum-yandex/codewars.js
@@ -38,3 +38,27 @@ reverseString('hello')
 function reverseString(str) {
   return str === '' ? '' : reverseString(str.substr(1)) + str.charAt(0)
 }
+
+// Check to see if a string has the same amount of 'x's and 'o's.
+// The method must return a boolean and be case insensitive. The string can contain any char.
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+function XO(str) {
+  let x = 0
+  let o = 0
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].toLowerCase() === 'x') {
+      x++
+    } else if (str[i].toLowerCase() === 'o') {
+      o++
+    }
+    return x === o
+  }
+}
