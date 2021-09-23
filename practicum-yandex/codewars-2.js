4ca43d3f491bd4d0a7a37aa38e0c5fe9c3d8bb0c
Ministrov <ministrov2018@gmail.com>
2021-09-23 19:21:25 +0300

    some changes to commit

--- practicum-yandex/codewars-2.js
+++ practicum-yandex/codewars-2.js
@@ -1,15 +1,38 @@
 // Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
 
-const strNumbers = '1234332456785586'
+let strNumbers = '1234332456785586'
 
 function fakeBin(x) {
-  for (let i = 0; i < strNumbers.length; i++) {
-    for (let j = 0; j < strNumbers.length; i++) {
-      if (strNumbers[i] < 5) {
-        return (strNumbers[j] = '0')
-      } else if (strNumbers >= 5) {
-        return (strNumbers[j] = '1')
+  let arr = x.split('')
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = +arr[i]
+    for (let j = i; j < arr.length; j++) {
+      if (arr[j] < 5) {
+        arr[j] = 0
       }
     }
+    if (arr[i] >= 5) {
+      arr[i] = 1
+    }
   }
+
+  return arr.join('')
+}
+
+console.log(fakeBin(strNumbers))
+
+// A second way to solve it
+
+function fakeBin(x) {
+  return x
+    .split('')
+    .map((n) => (n < 5 ? 0 : 1))
+    .join('')
+}
+
+//  The third way with a regex
+
+function fakeBin(x) {
+  return x.replace(/[0-4]/g, '0').replace(/[5-9]/g, '1')
 }
