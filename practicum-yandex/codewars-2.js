2d5c1858893543a7d27798f60c068f7cda9ed7ee
ministrov <61511030+ministrov@users.noreply.github.com>
2021-09-25 23:38:03 +0300

    resolved a tasks with kistay at codewars

--- practicum-yandex/codewars-2.js
+++ practicum-yandex/codewars-2.js
@@ -22,6 +22,10 @@ function fakeBin(x) {
 
 console.log(fakeBin(strNumbers))
 
+
+
+
+
 // A second way to solve it
 
 function fakeBin(x) {
@@ -36,3 +40,43 @@ function fakeBin(x) {
 function fakeBin(x) {
   return x.replace(/[0-4]/g, '0').replace(/[5-9]/g, '1')
 }
+
+
+// Write a function that will convert a string into title case, given an optional 
+// list of exceptions (minor words). The list of minor words will be given as a string 
+// with each word separated by a space. Your function should ignore the case of the minor
+//  words string -- it should behave in the same way even if the case of the minor word string is changed.
+
+
+// First argument (required): the original string to be converted.
+// Second argument (optional): space-delimited list of minor words 
+// that must always be lowercase except for the first word in the string. 
+// The JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
+
+function titleWord(word) {
+  const wordArr = word.split('');
+  wordArr[0] = wordArr[0].toUpperCase();
+  return wordArr.join('');
+}
+
+function titleCase(title, minorWords) {
+  if (title) {
+    title = title.toLowerCase();
+    const titleWords = title.split(' ');
+    titleWords[0] = titleWord(titleWords[0]);
+    let minorWordsArr = [];
+    if (minorWords) {
+      minorWords = minorWords.toLowerCase();
+      minorWordsArr = minorWords.split(' ');
+    }
+    for (let i = 1; i < titleWords.length; i++) {
+      if (!minorWordsArr.includes(titleWords[i])) {
+        titleWords[i] = titleWord(titleWords[i]);
+      }
+    }
+    return titleWords.join(' ');
+  }
+  return title;
+}
+
+console.log(titleCase('a clash of KINGS', 'a an the of'));
