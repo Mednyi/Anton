4d426012aafef6cf35c8e875505aff38060a26d3
Ministrov <ministrov2018@gmail.com>
2021-10-05 20:09:31 +0300

    Did a tasks from Scrimba

--- Next-level-of-JS/Es6.js
+++ Next-level-of-JS/Es6.js
@@ -82,3 +82,95 @@ describe('caseInsensitivePalindrome()', () => {
     expect(result).toBe(false)
   })
 })
+
+// Enclose in Brackets
+
+// Given a string, enclose it in round brackets
+
+function encloseInBrackets(str) {
+  const solution1 = '(' + str + ')'
+  const solution2 = `(${str})`
+  const solution3 = '('.concat(str, ')')
+  return solution1
+}
+
+/**
+ * Test Suite
+ */
+describe('encloseInBrackets()', () => {
+  it('adds () around a string', () => {
+    // arrange
+    const str = 'Yo'
+
+    // act
+    const result = encloseInBrackets(str)
+
+    // log
+    console.log('result: ', result)
+
+    // assert
+    expect(result).toBe('(Yo)')
+  })
+})
+
+// Number Factorial
+
+function factorialNumber(num) {
+  let sum = 1
+
+  for (let i = 1; i <= num; i++) {
+    sum *= i
+  }
+
+  return sum
+}
+
+/**
+ * Test Suite
+ */
+describe('factorialNumber()', () => {
+  it('returns factorial of number param', () => {
+    // arrange
+    const num = 5
+
+    // act
+    const result = factorialNumber(num)
+
+    // log
+    console.log('result: ', result)
+
+    // assert
+    expect(result).toBe(120)
+  })
+})
+
+// First Digits
+
+function firstDigit(str) {
+  const strNums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  const chars = str.split('')
+
+  for (char of chars) {
+    if (strNums.includes(char)) {
+      return char
+    }
+  }
+}
+/**
+ * Test Suite
+ */
+describe('firstDigit()', () => {
+  it('return the first digit in a string', () => {
+    // arrange
+    const str = 'var_1__Int2'
+
+    // act
+    const result = firstDigit(str)
+
+    // log
+    console.log('result: ', result)
+
+    // assert
+    expect(result).toBe('1')
+  })
+})
