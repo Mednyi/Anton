472eeed1d2403d0438ac31e6f66f1fb2f32da1e9
ministrov <61511030+ministrov@users.noreply.github.com>
2021-10-24 22:27:29 +0300

    some changes to commit

--- practicum-yandex/codewars-2.js
+++ practicum-yandex/codewars-2.js
@@ -124,3 +124,77 @@ function dirReduc(arr){
 }
 
 dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"])
+
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+// For example:
+
+// solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+// solution(null); // should return []
+
+function solution(nums){
+  if (!nums || nums.length === 0) {
+    return []
+  } else {
+    return nums.sort((a , b) => a - b)
+  }
+}
+
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+// Implement a function that determines whether a string that contains only letters 
+// is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+// "Dermatoglyphics" --> true
+// "aba" --> false
+// "moOse" --> false (ignore letter casing)
+
+function isIsogram(str) {
+  const strChanged = str.toLowerCase().split('');
+  for (let i = 0; i < strChanged.length; i++) {
+    for (let j = i + 1; j < strChanged.length; j++) {
+      if (strChanged[i] === strChanged[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+// A bookseller has lots of books classified in 26 categories labeled A, B, ... Z. Each book has a code c of 3, 4, 5 or more characters. The 1st character of a code is a capital letter which defines the book category.
+
+// In the bookseller's stocklist each code c is followed by a space and by a positive integer n (int n >= 0) which indicates the quantity of books of this code in stock.
+
+// For example an extract of a stocklist could be:
+
+// L = {"ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"}.
+// or
+// L = ["ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"] or ....
+// You will be given a stocklist (e.g. : L) and a list of categories in capital letters e.g :
+
+// M = {"A", "B", "C", "W"} 
+// or
+// M = ["A", "B", "C", "W"] or ...
+// and your task is to find all the books of L with codes belonging to each category of M and to sum their quantity according to each category.
+
+// For the lists L and M of example you have to return the string (in Haskell/Clojure/Racket a list of pairs):
+
+// (A : 20) - (B : 114) - (C : 50) - (W : 0)
+// where A, B, C, W are the categories, 20 is the sum of the unique book of category A, 114 the sum corresponding to "BKWRK" and "BTSQZ", 50 corresponding to "CDXEF" and 0 to category 'W' since there are no code beginning with W.
+
+// If L or M are empty return string is "" (Clojure and Racket should return an empty array/list instead).
+
+// Note:
+// In the result codes and their values are in the same order as in M
+
+function stockList(listOfArt, listOfCat){
+  const countBooks = listOfCat.reduce((acc, cat) => {
+    acc[cat] = 0;
+    return acc;
+  }, {})
+  for (let i = 0; i < listOfArt.length; i++) {
+    let [code, count] = listOfArt[i].split(' ');
+    countBooks[code] += count;
+  }
+
+  // Finish the task by yourself
+}
