1bdcb92a93418c88f6713b4f5673d70aa0228ffe
ministrov <61511030+ministrov@users.noreply.github.com>
2021-11-03 22:53:59 +0300

    Finished operating with codeWARS TASKS

--- practicum-yandex/codewars-3.js
+++ practicum-yandex/codewars-3.js
@@ -35,27 +35,6 @@ var maxSequence = function(arr){
 
 console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
 
-// I've done some changes to avoid a hardcoding
-
-var maxSequence = function(arr, allPositives, allNegatives, sum){
-  allPositives = arr => arr.every(n => n > 0)
-  allNegatives = arr => arr.every(n => n < 0)
-  sum = arr => arr.reduce((curr_max, max_so_far) => curr_max + max_so_far, 0)
-
-  if(arr.length === 0 || allNegatives(arr)) return 0;
-  if(allPositives(arr)) return sum(arr);
-
-  var curr_max = 0, max_so_far = 0;
-
-  for(var i = 0; i < arr.length; i++){  
-    curr_max = Math.max(0, curr_max + arr[i]);
-    max_so_far = Math.max(curr_max, max_so_far);
-  }
-  return max_so_far;
-}
-
-console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
-
 // Create a function that accepts dimensions, of Rows x Columns, as parameters 
 // in order to create a multiplication table sized according to the given dimensions. 
 // **The return value of the function must be an array, and the numbers must be Fixnums, NOT strings.
@@ -86,3 +65,57 @@ function multiplicationTable(row, col){
 }
 
 console.log(multiplicationTable(4, 5));
+
+// Prior to having fancy iPhones, teenagers would wear out their thumbs sending SMS messages on candybar-shaped feature phones with 3x4 numeric keypads.
+
+// ------- ------- -------
+// |     | | ABC | | DEF |
+// |  1  | |  2  | |  3  |
+// ------- ------- -------
+// ------- ------- -------
+// | GHI | | JKL | | MNO |
+// |  4  | |  5  | |  6  |
+// ------- ------- -------
+// ------- ------- -------
+// |PQRS | | TUV | | WXYZ|
+// |  7  | |  8  | |  9  |
+// ------- ------- -------
+// ------- ------- -------
+// |     | |space| |     |
+// |  *  | |  0  | |  #  |
+// ------- ------- -------
+// Prior to the development of T9 (predictive text entry) systems, the method to type words was called "multi-tap" and involved pressing a button repeatedly to cycle through the possible values.
+
+// For example, to type a letter "R" you would press the 7 key three times (as the screen display for the current character cycles through P->Q->R->S->7). A character is "locked in" once the user presses a different key or pauses for a short period of time (thus, no extra button presses are required beyond what is needed for each letter individually). The zero key handles spaces, with one press of the key producing a space and two presses producing a zero.
+
+// In order to send the message "WHERE DO U WANT 2 MEET L8R" a teen would have to actually do 47 button presses. No wonder they abbreviated.
+
+// For this assignment, write a module that can calculate the amount of button presses required for any phrase. Punctuation can be ignored for this exercise. Likewise, you can assume the phone doesn't distinguish between upper/lowercase characters (but you should allow your module to accept input in either for convenience).
+
+// Hint: While it wouldn't take too long to hard code the amount of keypresses for all 26 letters by hand, try to avoid doing so! (Imagine you work at a phone manufacturer who might be testing out different keyboard layouts, and you want to be able to test new ones rapidly.)
+
+
+function presses(phrase) {
+  phrase = phrase.toLowerCase();
+  let count = 0;
+  let index = -1;
+  let j = 0;
+  const keyboard = [
+    ["1"],["a", "b", "c", "2"], ["d", "e", "f", "3"],
+    ["g", "h", "i", "4"], ["j", "k", "l", "5"], ["m", "n", "o", "6"],
+    ["p", "q", "r", "s", "7"], ["t", "u", "v", "8"], ["w", "x", "y", "z", "9"],
+    ["*"], [" ", "0"], ["#"]
+  ]
+  for (let i = 0; i < phrase.length; i++) {
+    while (index === -1 && j < keyboard.length) {
+      index = keyboard[j].indexOf(phrase[i]);
+      j++;
+    }
+    count = count + index + 1;
+    index = -1;
+    j = 0;
+  }
+  return count;
+}
+
+console.log(presses("LOL"));
