7f9ebead0166fe1420ad81a6779df5942b0faeba
ministrov <61511030+ministrov@users.noreply.github.com>
2021-11-11 23:39:52 +0300

    got some resolved task

--- practicum-yandex/codewars-3.js
+++ practicum-yandex/codewars-3.js
@@ -119,3 +119,36 @@ function presses(phrase) {
 }
 
 console.log(presses("LOL"));
+
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+
+function alphabetPosition(text) {
+  text = text.toLowerCase(); // a
+  const textArr = text.split(''); // ['a']
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let result = []; 
+  for (let i = 0; i < textArr.length; i++) {
+    if (alphabet.includes(textArr[i])) {
+      result.push(alphabet.indexOf(textArr[i]) + 1);
+    }
+  }
+  return result.join(' ');   
+}
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock."));
+
+// The second way to solve it
+
+const text = "The sunset sets at twelve o' clock.";
+console.log(text.toLowerCase().replaceAll(/[^a-z]/g,'').split('').map(c => c.charCodeAt(0) - 96).join(' '))
+
