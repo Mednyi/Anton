b1ebb4230b420087e73c763874ae206cdf39f64a
Ministrov <ministrov2018@gmail.com>
2021-12-14 14:36:59 +0300

    Did some chages to commit

--- fourthProgramm.js
+++ fourthProgramm.js
@@ -3,12 +3,12 @@
 const numbersArr = [23, 34, 456, 31, 45, 567, 321, 907, 60, 78];
 
 function bubbleSort (arr) {
-  for (let j = numberArr.length - 1; j > 0; j--) {
+  for (let j = arr.length - 1; j > 0; j--) {
     for (let i = 0; i < j; i++) {
-      if (numberArr[i] > numberArr[i + 1]) {
-        let temp = numberArr[i];
-        numberArr[i] = numberArr[i + 1];
-        numberArr[i + 1] = temp;
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
       }
     }
   }
