5a9ce1a594ffd137ff214074ae04a32fa9baaf5a
ministrov <61511030+ministrov@users.noreply.github.com>
2022-01-16 16:31:02 +0300

    any changes with Number js

---
+++ Number.js
@@ -0,0 +1,12 @@
+// Get a random number
+
+const random = Number(Math.random().toFixed(2) * 10);
+
+// Get an integer from 0 to X(Where X is any of numbers) . 
+
+const X = 20;
+const randomX = Math.round(Math.random() * X);
+
+// Проверить результат вычисления 0.6 + 0.7 - как привести к нормальному виду
+
+const fixNum = Number(0.6 + 0.7).toFixed(1);
