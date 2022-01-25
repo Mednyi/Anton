aeb917a950b118f02db58d1e5c168651c70c5e9b
ministrov <61511030+ministrov@users.noreply.github.com>
2022-01-25 16:59:31 +0300

    Operating on high order functions

--- UdemyClasses/UdemyDenisCourses/array-methods.js
+++ UdemyClasses/UdemyDenisCourses/array-methods.js
@@ -42,4 +42,129 @@ function copySorted(arr) {
   return arr.slice().sort();
 }
 
-// 6 
+// 6 Создайте функцию конструктор Calculator, которая создаёт «расширяемые» объекты калькулятора.
+
+// Задание состоит из двух частей.
+
+//  a) Во - первых, реализуйте метод calculate(str), который принимает строку типа "1 + 2" в формате «ЧИСЛО оператор ЧИСЛО» (разделено пробелами) и возвращает результат.Метод должен понимать плюс + и минус -.
+
+//  b) Затем добавьте метод addMethod(name, func), который добавляет в калькулятор новые операции. Он принимает оператор name и функцию с двумя аргументами func(a,b), которая описывает его.
+
+ class Calculator  {
+
+  #methods = {
+    '-': (a, b) => a - b,
+    '+': (a, b) => a + b
+  };
+
+  #calculate = function(str) {
+
+    let split = str.split(' ');
+    let a = +split[0];
+    let op = split[1];
+    let b = +split[2];
+
+    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+      return NaN;
+    }
+
+    return this.methods[op](a, b);
+  }
+
+  #methods = function(name, func) {
+    this.methods[name] = func;
+  }
+}
+
+
+// 7 У вас есть массив объектов user, и в каждом из них есть user.name. Напишите код, который преобразует их в массив имён.
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha];
+
+let names = users.map( user => user.name);
+
+// 8 У вас есть массив объектов user, и у каждого из объектов есть name, surname и id.
+
+// Напишите код, который создаст ещё один массив объектов с параметрами id и fullName, где fullName – состоит из name и surname.
+
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 };
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+let users = [vasya, petya, masha];
+
+let usersTransformed = users.map( user => [ {id: user.id, fullName: user.name + user.surname}])
+
+let usersMapped = users.map(user => ({
+  fullName: `${user.name} ${user.surname}`,
+  id: user.id
+}));
+
+
+// 9 Напишите функцию sortByAge(users), которая принимает массив объектов со свойством age и сортирует их по нему.
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let arr = [vasya, petya, masha];
+
+function sortByAge() {
+  return arr.slice().sort((a, b) => a.age > b.age ? 1 : -1);
+}
+
+// 10 Напишите функцию shuffle(array), которая перемешивает (переупорядочивает случайным образом) элементы массива.
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5)
+}
+
+
+// алгоритм под названием Тасование Фишера — Йетса .Кроме того, если посмотреть с точки зрения производительности, то алгоритм «Тасование Фишера — Йетса» намного быстрее, так как в нём нет лишних затрат на сортировку.
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+
+// 11 Напишите функцию getAverageAge(users), которая принимает массив объектов со свойством age и возвращает средний возраст. Формула вычисления среднего арифметического значения: (age1 + age2 + ... + ageN) / N.
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 29 };
+
+let arr = [vasya, petya, masha];
+
+function getAverageAge() {
+  return arr.reduce((acc, item) => acc + item.age, 0) / arr.length;
+}
+
+
+
+// 12 Пусть arr – массив строк. Напишите функцию unique(arr), которая возвращает массив, содержащий только уникальные элементы arr.
+
+let strings = ["кришна", "кришна", "харе", "харе",
+  "харе", "харе", "кришна", "кришна", ":-O"
+];
+
+function unique(arr) {
+  let result = [];
+
+  for (let str of arr) {
+    if (!result.includes(str)) {
+      result.push(str);
+    }
+  }
+
+  return result;
+}
+
+function uniqueBestDecision(arr) {
+  return Array.from(new Set(arr));
+}
