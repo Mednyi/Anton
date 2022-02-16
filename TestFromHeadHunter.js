e9046629e30ac8b9ba38c0bb08c099be18ea2175
ministrov <61511030+ministrov@users.noreply.github.com>
2022-02-16 15:24:53 +0300

    Some changes

---
+++ TestFromHeadHunter.js
@@ -0,0 +1,89 @@
+const data = require('./data.json');
+
+// Функция которая получает отсортированный массив животных
+function getAnimalList(animal) {
+  return data.filter(item => item.type === animal);
+}
+
+function sortAnimalByAge(firstList, secondList, ...lists) {
+  
+}
+
+// Функция которая фильтрует массив животных поцвету
+function filterAnimalByColor(arr, color) {
+  return arr.filter(item => item.features.includes(color))
+}
+
+const task11Result = (animals) => {
+  const result = {};
+  // your code here
+
+  // Функция счетчик животных
+
+  function getAnimalQuantity(animal) {
+    return getAnimalList(animal).length
+  }
+
+  // Функция подсчета среднего возраста всех животных в массиве
+
+  function getAverageAge(arr) {
+    return Math.round(arr.reduce((acc, item) => acc + item.age, 0) / data.length)
+  }
+
+  // Функция создает объект
+
+  function createObj(dog, cat, avgage) {
+    return {
+      dog,
+      cat,
+      avgage,
+    }
+  }
+
+  return result = createObj(getAnimalQuantity('dog'), getAnimalQuantity('cat'), getAverageAge(date));
+};
+
+console.log(task11Result(data));
+
+const task12Result = (animals) => {
+  const result = 0;
+  // your code here
+
+  return result = getAnimalList(animals).filter(item => item.breed && item.features.includes('black')).length;
+};
+
+console.log(task12Result(data));
+
+const task13Result = (animals) => {
+  const result = [];
+  // your code here
+
+  return result = animals.concat(filterAnimalByColor(getAnimalList('dog', 'white'), filterAnimalByColor(getAnimalList('cat', 'black'))));
+};
+
+console.log(task13Result(data));
+
+const task14Result = (animals) => {
+  const result = [];
+  // your code here
+  return result;
+};
+
+console.log(task14Result(data));
+
+const myPowFunc = (number, n) => {
+  const result = number;
+  // your code here
+  return result;
+};
+
+console.log(myPowFunc(3, 4));
+
+const myFlatFunc = (inputArray) => {
+  const result = inputArray;
+  // your code here
+  return result;
+};
+
+console.log(myFlatFunc([1, 3, 5, [1, [4, 5], 'asdf', [76, [56, [66, 59]]]]]));
+// result 1, 3, 5, 1, 4, 5, 'asdf', 76, 56, 66, 59
