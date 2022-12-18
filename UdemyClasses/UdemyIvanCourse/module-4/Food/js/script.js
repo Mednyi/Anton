77d1d71b2a2d52c1a094e007ad9cc72edb8f1ce2
ministrov <61511030+ministrov@users.noreply.github.com>
2022-12-18 17:10:21 +0300

    feat: add module-3,4

---
+++ UdemyClasses/UdemyIvanCourse/module-4/Food/js/script.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabheader__item');
+  const tabsContent = document.querySelectorAll('.tabcontent');
+  const tabsParent = document.querySelector('.tabheader__items');
+
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      item.style.display = 'none';
+    });
+
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  }
+
+  function showTabContent(index = 0) {
+    tabsContent[index].style.display = 'block';
+    tabs[index].classList.add('tabheader__item_active');
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains('.tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+
+    console.log(target);
+  });
+});
