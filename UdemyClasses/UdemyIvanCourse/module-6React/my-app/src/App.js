b85e82beeb1fcda3d2072c34986d10121e5addcb
ministrov <61511030+ministrov@users.noreply.github.com>
2022-12-30 18:29:24 +0300

    feat: add react components

--- UdemyClasses/UdemyIvanCourse/module-6React/my-app/src/App.js
+++ UdemyClasses/UdemyIvanCourse/module-6React/my-app/src/App.js
@@ -1,15 +1,58 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+const Header = () => {
+  return <h2>Hello World!</h2>
+}
+
+const Field = () => {
+  const styledField = {
+    width: '300px'
+  };
+
+  return <input type="text" placeholder="Type here" style={styledField}/>
+}
+
+const Button = () => {
+  const text = 'Log in';
+  const logged = true;
+
+  return <button>{logged ? 'Enter' : text}</button>
+}
+
+const Countdown = () => {
+  return (
+    <div class="promotion__timer">
+      <div class="title">Осталось до конца акции:</div>
+      <div class="timer">
+          <div class="timer__block">
+            <span id="days">12</span>
+            дней
+          </div>
+          <div class="timer__block">
+            <span id="hours">20</span>
+            часов
+          </div>
+          <div class="timer__block">
+            <span id="minutes">56</span>
+            минут
+          </div>
+          <div class="timer__block">
+            <span id="seconds">20</span>
+            секунд
+          </div>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Header/>
+      <Field/>
+      <Button/>
+      <Countdown/>
     </div>
   );
 }
--- UdemyClasses/UdemyIvanCourse/module-6React/my-app/src/index.js
+++ UdemyClasses/UdemyIvanCourse/module-6React/my-app/src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 
-const element = <h2>Hello World!</h2>;
+// const element = <h2>Hello World!</h2>;
 
 // const element = React.createElement('h2', {className: 'greetings'}, 'Hello World!');
+
+// const text = 'Another World greetings!';
+
+// const element = (
+//   <div>
+//     <h2 className='text-element'> {text} </h2>
+//     <input type="text" />
+//     <button tabIndex={0}>Click</button>
+//   </div>
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
-  element
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
 
