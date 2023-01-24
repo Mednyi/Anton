e4249b94b6df9c4001c16d55e8b64a433028df1d
ministrov <61511030+ministrov@users.noreply.github.com>
2023-01-24 22:12:54 +0300

    feat: add router to project QuizeApp

---
+++ QuizApp/src/views/QuizesView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <header>
+      <h1>Quizes</h1>
+      <input v-model="search" type="text" placeholder="Search....">
+    </header>
+    <div class="options-container">
+      <Card
+        v-for="quiz in quizes"
+        :key="quiz.id"
+        :quiz="quiz"
+      />
+      <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
+        <img :src="quiz.img">
+        <div class="card-text">
+          <h2>{{ quiz.name }}</h2>
+          <p>{{ quiz.questions.length }} questions</p>
+        </div>
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import q from '../data/quizes.json'
+  import Card from '../components/Card.vue'
+  import { ref, watch } from 'vue';
+
+  const quizes = ref(q);
+  const search = ref('');
+
+  watch(search, () => {
+    quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()));
+  });
+
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  align-items: center;
+  margin-top: 30px;
+  margin-bottom: 10px;
+}
+
+header h1 {
+  margin-right: 30px;
+  font-weight: bold;
+}
+
+header input {
+  padding: 10px;
+  background-color: rgba(128, 128, 128, 0.1);
+  border: none;
+  border-radius: 5px;
+}
+
+.options-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40px;
+}
+</style>
