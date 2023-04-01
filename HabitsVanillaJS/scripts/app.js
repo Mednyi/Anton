7c7512e312d315552e352d9808e8fff33e212fea
ministrov <61511030+ministrov@users.noreply.github.com>
2023-04-01 14:50:37 +0300

    feat: operating on render content dinamically

--- HabitsVanillaJS/scripts/app.js
+++ HabitsVanillaJS/scripts/app.js
@@ -2,10 +2,20 @@
 
 let habbits = [];
 const HABBIT_KEY = 'HABBIT_KEY';
+let globalActiveHabbitId;
 
 /* page */
 const page = {
-  menu: document.querySelector('.menu__list')
+  menu: document.querySelector('.menu__list'),
+  header: {
+    h1: document.querySelector('.h1'),
+    progressPersent: document.querySelector('.progress__percent'),
+    progressCoverBar: document.querySelector('.progress__cover-bar')
+  },
+  content: {
+    daysContainer: document.querySelector('#days'),
+    nextDay: document.querySelector('.habbit__day')
+  }
 };
 
 /* utils */
@@ -24,10 +34,6 @@ function saveData() {
 
 /* render */
 function rerenderMenu(activeHabbit) {
-  if (!activeHabbit) {
-    return;
-  }
-
   for (const habbit of habbits) {
     const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
     if (!existed) {
@@ -50,9 +56,78 @@ function rerenderMenu(activeHabbit) {
   }
 }
 
+function rerenderHead(activeHabbit) {
+  page.header.h1.innerText = activeHabbit.name;
+  const progress = activeHabbit.days.length / activeHabbit.target > 1
+    ? 100
+    : activeHabbit.days.length / activeHabbit.target * 100;
+  page.header.progressPersent.innerText = progress.toFixed(0) + ' %';
+  page.header.progressCoverBar.setAttribute('style', `width: ${progress}%`)
+}
+
+function rerenderContent(activeHabbit) {
+  page.content.daysContainer.innerHTML = '';
+  for (const index in activeHabbit.days) {
+    const element = document.createElement('div');
+    element.classList.add('habbit');
+    element.innerHTML = `<div class="habbit__day">День ${Number(index) + 1}</div>
+      <div class="habbit__comment">${activeHabbit.days[index].comment}</div>
+      <button class="habbit__delete" onclick="deleteDay(${index})">
+        <img src="images/delete.svg" width="16" height="17" alt="Удалить день ${index + 1}">
+      </button>`
+    page.content.daysContainer.appendChild(element);
+    page.content.nextDay.innerHTML = `День ${activeHabbit.days.length + 1}`;
+  }
+}
+
 function rerender(activeHabbitId) {
+  globalActiveHabbitId = activeHabbitId;
   const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
+  if (!activeHabbit) {
+    return;
+  }
   rerenderMenu(activeHabbit);
+  rerenderHead(activeHabbit);
+  rerenderContent(activeHabbit);
+}
+
+/* work with days */
+function addDays(event) {
+  const form = event.target;
+  event.preventDefault();
+  const data = new FormData(form);
+  const comment = data.get('comment');
+  form['comment'].classList.remove('error');
+  if (!comment) {
+    form['comment'].classList.add('error');
+  }
+  habbits = habbits.map(habbit => {
+    if (habbit.id === globalActiveHabbitId) {
+      return {
+        ...habbit,
+        days: habbit.days.concat([{ comment}])
+      }
+    }
+    return habbit;
+  });
+  form['comment'].value = '';
+  rerender(globalActiveHabbitId);
+  saveData();
+}
+
+function deleteDay(index) {
+  habbits = habbits.map(habbit => {
+    if (habbit.id === globalActiveHabbitId) {
+      habbit.days.splice(index, 1);
+      return {
+        ...habbit,
+        days: habbit.days
+      };
+    }
+    return habbit;
+  })
+  rerender(globalActiveHabbitId);
+  saveData();
 }
 
 /* init */
