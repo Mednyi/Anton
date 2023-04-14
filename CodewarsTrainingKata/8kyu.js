1d4d0324dcebb147a278c9f4b20909225f792b99
ministrov <61511030+ministrov@users.noreply.github.com>
2023-04-14 21:00:56 +0300

    feat: add new tasks

--- CodewarsTrainingKata/8kyu.js
+++ CodewarsTrainingKata/8kyu.js
@@ -1788,7 +1788,45 @@ function firstNonConsecutive(arr) {
 //   + 9 dog years for second year
 //     + 5 dog years for each year after that
 
-var humanYearsCatYearsDogYears = function (humanYears) {
-  // Your code here!
-  return [0, 0, 0];
+const humanYearsCatYearsDogYears = function (humanYears) {
+  // return humanYears, catYears, dogYears
+  let catYears = 0;
+  let dogYears = 0;
+
+  for (let i = 1; i <= humanYears; i++) {
+    if (humanYears === 1) {
+      catYears = 15;
+      dogYears = 15;
+    } else if (humanYears === 2) {
+      catYears = 24;
+      dogYears = 24;
+    } else if (humanYears >= 3) {
+      catYears += 4;
+      dogYears += 5;
+    }
+  }
+  return [humanYears, catYears, dogYears];
+}
+
+const humanYearsCatYearsDogYears2 = function (humanYears) {
+  if (humanYears == 1) return [1, 15, 15];
+  if (humanYears == 2) return [2, 24, 24];
+
+  return [
+    humanYears,
+    24 + (humanYears - 2) * 4,
+    24 + (humanYears - 2) * 5
+  ]
+}
+
+function humanYearsCatYearsDogYears(humanYears) {
+  switch (humanYears) {
+    case 1:
+      return [1, 15, 15]
+    case 2:
+      return [2, 24, 24];
+    default:
+      return [humanYears, (humanYears - 2) * 4 + 24, (humanYears - 2) * 5 + 24];
+  }
+
 }
