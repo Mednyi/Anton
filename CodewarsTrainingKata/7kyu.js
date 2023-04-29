6269e3280daaa55c2452170ada1ab319d22e0f36
ministrov <61511030+ministrov@users.noreply.github.com>
2023-04-29 21:35:27 +0300

    feat: add new task

--- CodewarsTrainingKata/7kyu.js
+++ CodewarsTrainingKata/7kyu.js
@@ -450,11 +450,16 @@ function sortByLength(array) {
 // [17, 17, 3, 17, 17, 17, 17] ==> 3
 
 function stray(numbers) {
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] == numbers[i + 1]) {
-      break;
-    } else {
-      return numbers[i];
-    }
+  //take first element
+  let strayChar = numbers[0];
+
+  // see if it is different from the next 2
+  if (strayChar !== numbers[1] && strayChar !== numbers[2]) return strayChar;
+
+  // if not find the first value that is different
+  for (let i = 1; i < numbers.length; i++) {
+    if (strayChar !== numbers[i]) return numbers[i];
   }
 }
+
+const stray = numbers => numbers.find(num => numbers.indexOf(num) === numbers.lastIndexOf(num));
--- CodewarsTrainingKata/8kyu.js
+++ CodewarsTrainingKata/8kyu.js
@@ -1867,3 +1867,21 @@ function expressionMatter(a, b, c) {
   arr.push(a * (b + c));
   return arr.reduce((a, b) => Math.max(a, b));
 }
+
+// 66
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+// Example:
+// get_char(65)
+// should return:
+// 'A'
+
+function getChar(c) {
+  return String.fromCharCode(c);
+}
+
+// 67
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+function binToDec(bin) {
+  // ...
+}
