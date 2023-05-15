decbdf83e4a6290efd795def7c2c59b54346efd9
ministrov <61511030+ministrov@users.noreply.github.com>
2023-05-15 13:17:23 +0300

    feat: develop a budget project

--- BudgetVanillaJS/js/main.js
+++ BudgetVanillaJS/js/main.js
@@ -1,29 +1,131 @@
 'use strict';
 
-const start = document.getElementById('start');
-const budget = document.getElementsByClassName('budget-value');
-const daybudget = document.getElementsByClassName('daybudget-value');
-const level = document.getElementsByClassName('level-value');
-const expenses = document.getElementsByClassName('expenses-value');
-const optionalexpenses = document.getElementsByClassName('optionalexpenses-value');
-const income = document.getElementsByClassName('income-value');
-const monthsavings = document.getElementsByClassName('monthsavings-value');
-const yearsavings = document.getElementsByClassName('yearsavings-value');
+let startBtn = document.getElementById("start"),
+  budgetValue = document.getElementsByClassName('budget-value')[0],
+  dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+  levelValue = document.getElementsByClassName('level-value')[0],
+  expensesValue = document.getElementsByClassName('expenses-value')[0],
+  optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+  incomeValue = document.getElementsByClassName('income-value')[0],
+  monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+  yearSavingsValue = document.getElementsByClassName('yearsavings-value')[0],
 
-const expensesItem = document.querySelectorAll('.expenses-item');
-const optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item');
 
-const optionalexpensesBtn = document.querySelector('.optionalexpenses-btn');
-const countBudgetBtn = document.querySelector('.count-budget-btn');
+  expensesItem = document.getElementsByClassName('expenses-item'),
+  expensesBtn = document.getElementsByTagName('button')[0],
+  optionalExpensesBtn = document.getElementsByTagName('button')[1],
+  countBtn = document.getElementsByTagName('button')[2],
+  optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+  incomeItem = document.querySelector('.choose-income'),
+  checkSavings = document.querySelector('#savings'),
+  sumValue = document.querySelector('.choose-sum'),
+  percentValue = document.querySelector('.choose-percent'),
+  yearValue = document.querySelector('.year-value'),
+  monthValue = document.querySelector('.month-value'),
+  dayValue = document.querySelector('.day-value');
 
-const chooseIncome = document.querySelector('.choose-income');
+let money, time;
 
-const savings = document.getElementById('savings');
-const sum = document.getElementById('sum');
-const percent = document.getElementById('percent');
+startBtn.addEventListener('click', function() {
+  time = prompt("Введите дату в формате YYYY-MM-DD", "");
+  money = +prompt("Ваш бюджет на месяц?", "");
 
-const yearValue = document.querySelector('.year-value');
-const monthValue = document.querySelector('.month-value');
-const dayValue = document.querySelector('.day-value');
+  while (isNaN(money) || money == "" || money == null) {
+    money = +prompt("Ваш бюджет на месяц?", "");
+  }
 
-console.log(savings);
+  appData.budget = money;
+  appData.timeData = time;
+  budgetValue.textContent = money.toFixed();
+  yearValue.value = new Date(Date.parse(time)).getFullYear();
+  monthValue.value = new Date(Date.parse(time)).getMonth() + 1;
+  dayValue.value = new Date(Date.parse(time)).getDay();
+});
+
+expensesBtn.addEventListener('click', function() {
+  let sum = 0;
+
+  for (let i = 0; i < expensesItem.length; i++) {
+    let a = expensesItem[i].value, /* Get data from user */
+      b = expensesItem[++i].value; /* Get data from user */
+
+    if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != "" && b != "" && a.length < 50) {
+
+      console.log("done");
+
+      appData.expenses[a] = b; /* Write data from user to expences object */
+      sum += +b;
+    } else {
+      console.log("bad result");
+      i--;
+    }
+  }
+
+  expensesValue.textContent = sum;
+});
+
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: true,
+  chooseExpenses: function () {
+
+  },
+  detectDayBudget: function () {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+  },
+  detectLevel: function () {
+    if (appData.moneyPerDay < 100) {
+      console.log("Это минимальный уровень достатка!");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+      console.log("Это средний уровень достатка!");
+    } else if (appData.moneyPerDay > 2000) {
+      console.log("Это высокий уровень достатка!");
+    } else {
+      console.log("Ошибочка...!");
+    }
+  },
+  checkSavings: function () {
+    if (appData.savings == true) {
+      let save = +prompt("Какова сумма накоплений?"),
+        percent = +prompt("Под какой процент?");
+
+      appData.monthIncome = save / 100 / 12 * percent;
+      alert("Доход с Вашего депозита в месяц: " + appData.monthIncome);
+    }
+  },
+  chooseOptExpenses: function () {
+    for (let i = 1; i <= 3; i++) {
+      let questionOptExpenses = prompt("Статья необязательных расходов?");
+      appData.optionalExpenses[i] = questionOptExpenses;
+      console.log(appData.optionalExpenses);
+    }
+  },
+  chooseIncome: function () {
+
+    let items = prompt("Что принесет дополнительный доход? (Перечислите через запятую)", "");
+
+    if (typeof (items) != "string" || items == "" || typeof (items) == null) {
+      console.log("Вы ввели некорректные данные или не ввели их вовсе");
+    } else {
+      appData.income = items.split(", ");
+      appData.income.push(prompt("Может что-то еще?"));
+      appData.income.sort();
+    }
+
+    appData.income.forEach(function (itemmassive, i) {
+      alert("Способы доп. заработка: " + (i + 1) + " - " + itemmassive);
+    });
+
+  }
+
+
+};
+
+for (let key in appData) {
+  console.log("Наша программа включает в себя данные: " + key + " - " + appData[key]);
+}
