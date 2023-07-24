64649b4977a14537b129edfb5609e53e56e5dda8
ministrov <61511030+ministrov@users.noreply.github.com>
2023-07-24 19:14:46 +0300

    feat: operate with modal

---
+++ UdemyClasses/UdemyIvanCourse/module-10Practical/Source/src/js/main.js
@@ -0,0 +1,9 @@
+import './slider';
+import modals from './modules/modals';
+
+window.addEventListener('DOMContentLoaded', () => {
+  modals();
+
+  console.log('Hi, my name is Anton and I am the best frontend developer ever');
+});
+
