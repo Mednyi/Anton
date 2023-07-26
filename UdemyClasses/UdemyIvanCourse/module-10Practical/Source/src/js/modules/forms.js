5ac517528b278d1c3f6f76eae8b006d5727d738e
ministrov <61511030+ministrov@users.noreply.github.com>
2023-07-26 17:45:51 +0300

    feat: operate with formData

--- UdemyClasses/UdemyIvanCourse/module-10Practical/Source/src/js/modules/forms.js
+++ UdemyClasses/UdemyIvanCourse/module-10Practical/Source/src/js/modules/forms.js
@@ -1,5 +1,61 @@
 const forms = () => {
+  const form = document.querySelectorAll('form'),
+        input = document.querySelectorAll('input'),
+        phoneInputs = document.querySelectorAll('input[name="user_phone"]');
 
+  phoneInputs.forEach(item => {
+    item.value = item.value.replace(/\D/, '');
+  });
+
+  const message = {
+    loading: 'Загрузка....',
+    success: 'Спасибо! Мы с вами скоро свяжемся.',
+    failure: 'Что-то пошло не так....'
+  };
+
+  const postData = async (url, data) => {
+    document.querySelector('.status').textContent = message.loading;
+    let res = await fetch(url, {
+      method: 'POST',
+      body: data
+    });
+
+    return await res.text();
+  }
+
+  const clearInputs = () => {
+    input.forEach(item => {
+      item.value = '';
+    });
+  };
+
+  form.forEach(item => {
+    item.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      let statusMessage = document.createElement('div');
+      statusMessage.classList.add('.status');
+      item.appendChild(statusMessage);
+
+      const formData = new FormData(item);
+
+      console.log(...formData);
+      console.log(typeof formData);
+
+      postData('assets/server.php', formData)
+        .then(result => {
+          console.log(result);
+          statusMessage.textContent = message.success;
+        })
+        .catch(() => statusMessage.textContent = message.failure)
+        .finally(() => {
+          clearInputs();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 5000);
+        })
+    });
+  });
 };
 
 export default forms;
