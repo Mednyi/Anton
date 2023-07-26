4cf8e501559aa7ba91cbb57998bc31e68100e786
ministrov <61511030+ministrov@users.noreply.github.com>
2023-07-26 14:20:24 +0300

    feat: operating with fibonacci sequence

--- PurpleSchoolAdvancedJS/MapAndSet/Sets.js
+++ PurpleSchoolAdvancedJS/MapAndSet/Sets.js
@@ -5,7 +5,7 @@ const flights = ['Russia', 'USA', 'London', 'London', 'USA'];
 const setFlights = new Set(flights);
 console.log(setFlights);
 console.log(setFlights.size);
-console.log(setFlights.had('Russia'));
+console.log(setFlights.has('Russia'));
 console.log(setFlights.add('Paris'));
 console.log(setFlights.delete('London'));
 console.log(setFlights.clear());
@@ -19,3 +19,16 @@ for (const flight of setFlights) {
 // From Set to the array
 
 console.log([...setFlights]);
+
+
+const javaScriptTypes = [1, 'string', false, 1, 'string', false, false, 1, undefined, undefined, undefined, null, null, null, null];
+
+const uniqueArray = new Set(javaScriptTypes);
+
+console.log(uniqueArray);
+
+for (const item of uniqueArray) {
+  console.log(item);
+}
+
+console.log([...uniqueArray]);
