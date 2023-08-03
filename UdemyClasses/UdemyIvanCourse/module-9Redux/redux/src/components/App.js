a7c975897a2a68f5944ef17e17d94b517152b0c7
ministrov <61511030+ministrov@users.noreply.github.com>
2023-08-03 18:19:46 +0300

    feat: keep on operating with react-redux

---
+++ UdemyClasses/UdemyIvanCourse/module-9Redux/redux/src/components/App.js
@@ -0,0 +1,19 @@
+import Counter from "./Counter";
+
+
+const App = () => {
+  return (
+    <Counter
+      // counter={getState().value}
+      // inc={inc}
+      // dec={dec}
+      // res={res}
+      // rnd={() => {
+      //   const value = Math.floor(Math.random() * 10);
+      //   rnd(value);
+      // }}
+    />
+  )
+}
+
+export default App;
--- UdemyClasses/UdemyIvanCourse/module-9Redux/redux/src/index.js
+++ UdemyClasses/UdemyIvanCourse/module-9Redux/redux/src/index.js
@@ -1,70 +1,28 @@
 import React from 'react';
-import { legacy_createStore as createStore } from 'redux';
 import ReactDOM from 'react-dom/client';
-
-const initialState = { value: 0 };
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "INC":
-      return {
-        ...state,
-        value: state.value + 1
-      };
-    case "DEC":
-      return {
-        ...state,
-        value: state.value - 1
-      };
-    case "RND":
-      return {
-        ...state,
-        value: state.value * action.payload
-      };
-    default:
-      return state;
-  }
-}
+import { Provider } from 'react-redux';
+import App from './components/App';
+import { legacy_createStore as createStore } from 'redux';
+import reducer from './reducer';
+// import * as actions from './actions';
 
 const store = createStore(reducer);
+// const { dispatch, subscribe, getState } = store;
 
-const update = () => {
-  document.getElementById('counter').textContent = store.getState().value;
-}
+// const { inc, dec, rnd, res } = bindActionCreators(actions, dispatch);
 
-store.subscribe(update);
-
-const inc = () => ({ type: 'INC' });
-const dec = () => ({ type: 'DEC' });
-const rnd = (value) => ({ type: 'RND', payload: value });
-
-document.getElementById('inc').addEventListener('click', () => {
-  store.dispatch(inc());
-});
-
-document.getElementById('dec').addEventListener('click', () => {
-  store.dispatch(dec());
-});
-
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  store.dispatch(rnd(value));
-});
-
-// let state = reducer(initialState, { type: 'INCREAMENT'});
-// state = reducer(state, { type: 'INCREAMENT'});
-// state = reducer(state, { type: 'INCREAMENT'});
-// state = reducer(state, { type: 'INCREAMENT'});
-
-// console.log(state);
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <>
-      {initialState}
-      <h1>Hello World</h1>
-    </>
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//   dispatch(creator(...args));
+// }
+
+
+
